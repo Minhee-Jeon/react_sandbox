cb38efabe8b8d3084840ceb49cf9282ab8572e41
test <test@testui-MacBookPro.local>
2022-01-31 15:04:04 +0900

    this.setState가 끝난 후 특정 작업 실행하기
    setState로 값 업데이트 후에 특정 작업을 하고 싶으면 setState의 두 번째 파라미터로 콜백함수를 등록해 작업을 처리할 수 있다.

--- src/Counter.js
+++ src/Counter.js
@@ -14,16 +14,15 @@ export default class Counter extends Component {
         <button
           onClick={() => {
             //this.setState로 state에 새 값을 넣을 수 있다.
-            this.setState((prevState) => {
-              return {
-                number: prevState.number + 1,
-              };
-            });
-            // 위 코드와 아래 코드는 완전히 똑같은 기능을 한다.
-            // 아래 코드는 함수에서 바로 객체를 반환한다는 의미
-            this.setState((prevState) => ({
-              number: prevState.number + 1,
-            }));
+            this.setState(
+              {
+                number: number + 1,
+              },
+              () => {
+                console.log("방금 setState가 호출되었습니다");
+                console.log(this.state);
+              }
+            );
           }}
         >
           +1
