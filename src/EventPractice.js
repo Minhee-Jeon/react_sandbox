29fcb13dd56bfd30971e6c3de43e5cff234f54e6
test <test@testui-MacBookPro.local>
2022-02-01 23:33:03 +0900

    렌더링 동시에 함수 만들어 전하기 X, 함수를 미리 준비해 전달하기 O
    성능상엔 별 차이 없지만 가독성은 좋다. 상황에 따라 렌더링 메서드 내부에 함수를 만드는 것이 더 좋을 때도 있음

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -4,6 +4,20 @@ export default class EventPractice extends Component {
   state = {
     message: "",
   };
+
+  handleChange = (e) => {
+    this.setState({
+      message: e.target.value,
+    });
+  };
+
+  handleClick = () => {
+    alert(this.state.message);
+    this.setState({
+      message: "",
+    });
+  };
+
   render() {
     return (
       <div>
@@ -13,22 +27,9 @@ export default class EventPractice extends Component {
           name="message"
           placeholder="아무거나 입력해 보세요"
           value={this.state.message}
-          onChange={(e) => {
-            this.setState({
-              message: e.target.value,
-            });
-          }}
+          onChange={this.handleChange}
         />
-        <button
-          onClick={() => {
-            alert(this.state.message);
-            this.setState({
-              message: "",
-            });
-          }}
-        >
-          확인
-        </button>
+        <button onClick={this.handleClick}>확인</button>
       </div>
     );
   }
