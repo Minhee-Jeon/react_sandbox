0b5b007fb6cd17379014257327fb73ed4c3ebf7a
test <test@testui-MacBookPro.local>
2022-02-05 12:02:53 +0900

    에러 잡아내기
    에러가 발생하면 componentDidCatch 메서드가 호출된다. ErrorBoundatry컴포넌트에서 이 메서드를 이용해 에러 발생시 알림 문구를 보여주도록 설정했다.

--- src/LifeCycleSample.js
+++ src/LifeCycleSample.js
@@ -66,6 +66,7 @@ export default class LifeCycleSample extends Component {
     };
     return (
       <div>
+        {this.props.missing.value}
         <h1 style={style} ref={(ref) => (this.MyRef = ref)}>
           {this.state.number}
         </h1>
