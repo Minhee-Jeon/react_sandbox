31fef0dba90e4deeab8255058d01877996780927
minhee <jmh3699@naver.com>
2023-08-03 23:56:32 +0900

    basic03 Props를 사용해 Profile 컴포넌트의 재사용성을 높여보기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -4,9 +4,21 @@ import Profile from './components/Profile';
 function AppProfile() {
   return (
     <>
-      <Profile />
-      <Profile />
-      <Profile />
+      <Profile 
+        image='https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
+        name='Alice Lee'
+        title='프론트엔드 개발자'
+      />
+      <Profile 
+        image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
+        name='Anna Young'
+        title='백엔드 개발자'
+      />
+      <Profile 
+        image='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1160&q=80'
+        name='Bob Kim'
+        title='서비스 기획자'
+      />
     </>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-export default function Profile() {
+export default function Profile({ image, name, title }) {
   return (
     <div className='profile'>
       <img 
         className='photo'
-        src="https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80" 
+        src={image}
         alt="avatar" 
       />
-      <h1>Alice Lee</h1>
-      <p>프론트엔드 개발자</p>
+      <h1>{name}</h1>
+      <p>{title}</p>
     </div>
   );
 }
