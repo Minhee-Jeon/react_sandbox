b43043b6efafe64b452c6380a412f4dbfd57af59
minhee <jmh3699@naver.com>
2023-08-05 23:58:13 +0900

    basic04 isNew 속성이 true인 Profile에만 태그 표시하기

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -8,6 +8,7 @@ function AppProfile() {
         image='https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80'
         name='Alice Lee'
         title='프론트엔드 개발자'
+        isNew={true}
       />
       <Profile 
         image='https://images.unsplash.com/photo-1580489944761-15a19d654956?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=922&q=80'
